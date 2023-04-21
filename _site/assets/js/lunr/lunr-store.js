28167ff5bb55f0ac2405b641b31772ff69829297
byeonghwi <hwi@byeonghwi-MacBookAir.local>
2023-04-21 09:15:08 +0900

    Github blog update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Layout: Header Video",
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=-PVofD2A9t8 (long version) or https://youtu.be/-PVofD2A9t8...","categories": ["Layout","Uncategorized"],
+        "tags": ["video","layout"],
+        "url": "/layout/uncategorized/layout-header-video/",
+        "teaser": null
+      },{
+        "title": "깃허브 테스트 중",
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Settings Parameter Required Description id Required ID of the video provider Required Hosting provider of the video, either youtube or vimeo YouTube To embed the following YouTube video at url https://www.youtube.com/watch?v=-PVofD2A9t8 (long version) or https://youtu.be/-PVofD2A9t8...","categories": ["Layout","Uncategorized"],
+        "tags": ["video","layout"],
+        "url": "/layout/uncategorized/test/",
+        "teaser": null
+      }]
