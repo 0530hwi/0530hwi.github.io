a241d07c9434abf823b69d722a317a4f59e2e49a
byeonghwi <hwi@byeonghwi-MacBookAir.local>
2023-04-21 16:39:39 +0900

    Testing Blog

--- _posts/2023-04-21-Make_GitBlog.md
+++ _posts/2023-04-21-Make_GitBlog.md
@@ -3,13 +3,11 @@ title: "깃허브 블로그 만들기 PART 1. 사전 준비"
 excerpt: "깃허브 블로그 만들기, 정말 쉽게 정리했습니다. (에러 시 문제 해결 방법)"
 
 categories:
-    - Blog
+    - blog
 tags:
     - [Blog, Git, Github, Jekyll, Brew, Ruby, rbenv]
-
-toc: true
-toc_sticky: true
-
 date: 2023-04-21
 last_modified_at: 
 ---
+
+테스트 중입니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "깃허브 블로그 만들기 PART 1. 사전 준비",
-        "excerpt":" ","categories": ["Blog"],
+        "excerpt":"테스트 중입니다.  ","categories": ["blog"],
         "tags": ["Blog","Git","Github","Jekyll","Brew","Ruby","rbenv"],
         "url": "/blog/Make_GitBlog/",
         "teaser": null
