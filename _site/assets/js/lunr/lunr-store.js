9cdad8cc24d146d34e999a1bcbc7b35d61f4f1a1
byeonghwi <hwi@byeonghwi-MacBookAir.local>
2023-04-21 18:31:10 +0900

    update Blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "깃허브 블로그 만들기 PART 1. 사전 준비",
-        "excerpt":" ","categories": ["Blog"],
+        "title": "깃허브 블로그 커스텀 PART 1. 구조 파악",
+        "excerpt":" ","categories": ["Custom Blog"],
         "tags": ["Blog","Git","Github","Jekyll","Brew","Ruby","rbenv"],
-        "url": "/blog/Make_GitBlog/",
+        "url": "/custom%20blog/Custom_GitBlog/",
+        "teaser": null
+      },{
+        "title": "깃허브 블로그 만들기 PART 1. 사전 준비",
+        "excerpt":" ","categories": ["Making Blog"],
+        "tags": ["Blog","Git","Github","Jekyll","Brew","Ruby","rbenv"],
+        "url": "/making%20blog/Make_GitBlog/",
         "teaser": null
       }]
