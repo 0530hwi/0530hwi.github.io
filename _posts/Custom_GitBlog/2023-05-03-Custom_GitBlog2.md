d0ee564e83d92e12dce1c679cb4a2f17f9569358
byeonghwi <hwi@byeonghwi-MacBookAir.local>
2023-05-03 13:20:14 +0900

    Change Font and Posting and Delete Font file

---
+++ _posts/Custom_GitBlog/2023-05-03-Custom_GitBlog2.md
@@ -0,0 +1,30 @@
+---
+title: "깃허브 블로그 커스텀하기 - 블로그 폰트 변경하기"
+excerpt: "깃허브 블로그 커스텀, 쉽게 정리했습니다. '깃허브 블로그 폰트 변경하기"
+
+categories:
+    -  Custom Blog
+tags:
+    - [Blog, Git, Github, Jekyll, Brew, Ruby, rbenv]
+
+toc: true
+toc_sticky: true
+
+date: 2023-05-03
+last_modified_at: 
+comments: true
+---
+<br>
+
+<div align=center>
+
+
+❗️커스텀 포스팅은 필자의 시행착오와 독학을 통해 작성되었습니다.❗️<br> 틀린 부분이나 좀 더 쉬운 방법이 있다면 댓글 부탁드려요. 다양한 피드백 환영입니다😃
+
+
+</div>
+
+<br><br>
+
+# 폰트 변경하는 방법
+✅**참고**: 블로그의 폰트를 변경하는 방법은 여러가지가 있습니다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -28,4 +28,10 @@ var store = [{
         "tags": ["Blog","Git","Github","Jekyll","Brew","Ruby","rbenv"],
         "url": "/custom%20blog/Custom_GitBlog1/",
         "teaser": null
+      },{
+        "title": "깃허브 블로그 커스텀하기 - 블로그 폰트 변경하기",
+        "excerpt":"       ❗️커스텀 포스팅은 필자의 시행착오와 독학을 통해 작성되었습니다.❗️  틀린 부분이나 좀 더 쉬운 방법이 있다면 댓글 부탁드려요. 다양한 피드백 환영입니다😃          폰트 변경하는 방법  ✅참고: 블로그의 폰트를 변경하는 방법은 여러가지가 있습니다.  ","categories": ["Custom Blog"],
+        "tags": ["Blog","Git","Github","Jekyll","Brew","Ruby","rbenv"],
+        "url": "/custom%20blog/Custom_GitBlog2/",
+        "teaser": null
       }]
